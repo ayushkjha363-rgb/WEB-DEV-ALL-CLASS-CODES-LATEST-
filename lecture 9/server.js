6255158df6345b530831c7cc447b6544b4a8167d
Ayush <ayushkjha363@gmail.com>
2026-09-19 17:23:01 +0530

    Update server.js

--- lecture 9/server.js
+++ lecture 9/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 
 const users=[
-    {id:1, name:"Devendra", email:"GZDlC@example.com"},
+    {id:1, name :"Devendra", email:"GZDlC@example.com"},
     {id:2, name:"Ravi", email:"ravi@com"},
     {id:3, name:"alex", email:"alex@com"},
     {id:4, name:"vasu", email:"vasu@com"},
